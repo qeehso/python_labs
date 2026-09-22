df5cd54815540d5b7d1ac9faaff04b1ce7fa40f8
Никита <nicksmith222777@gmail.com>
2026-09-22 15:47:52 +0300

    .

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -1,15 +1,19 @@
 def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
-    """
-    Возвращает кортеж, содержащий минимальное и максимальное значение из списка чисел
 
-    """
     if not nums:
-        raise ValueError("Список не должен быть пустым")
+        raise ValueError('Список не должен быть пустым')
     
-    min_value = min(nums)
-    max_value = max(nums)
+    minimum = nums[0]
+    maximum = nums[0]
 
-    return min_value, max_value
+    for i in range(1, len(nums)):
+
+        if nums[i] < minimum:
+            minimum = nums[i]
+        if nums[i] > maximum:
+            maximum = nums[i]
+            
+    return minimum, maximum
 
     
 
@@ -18,8 +22,22 @@ def unique_sorted(nums: list[float | int]) -> list[float | int]:
     Возвращает отсортированный список уникальных значений по возрастанию
 
     """
-    
-    return sorted(set(nums))
+    seen = []
+
+    for num in nums:
+        if num not in seen:
+            seen.append(num)
+
+    for i in range(len(seen)):
+        min_ind = i
+
+        for j in range(i + 1, len(seen)):
+            if seen[j] < seen[min_ind]:
+                min_ind = j
+
+        seen[i], seen[min_ind] = seen[min_ind], seen[i]
+
+    return seen
 
 
 def flatten(mat: list[list | tuple]) -> list:
@@ -34,4 +52,3 @@ def flatten(mat: list[list | tuple]) -> list:
             result.append(element)
 
     return result
-
